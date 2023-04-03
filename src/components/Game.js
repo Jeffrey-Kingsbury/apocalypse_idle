12dafc4285cbca0a4cfa27c3ef0fbe11bb5c6f3f
Jeffrey Kingsbury <109102474+Jeffrey-Kingsbury@users.noreply.github.com>
2023-04-02 22:39:44 -0400

    large changes
    Add wallet, health, and equipped item functionality to player object
    
    - Add wallet, max_health, health, and equipped properties to DEFAULT_PLAYER
    - Update LOAD_PLAYER function:
      - Add default values for wallet, max_health, health, and equipped
      - Update max_health based on the constitution skill level
    - Create UPDATE_HEALTH function for modifying player health
    - Create UPDATE_WALLET function for modifying player wallet
    - Create UPDATE_EQUIPPED function for equipping items
    - Create UNEQUIP_ITEM function for unequipping items
    - Remove filtering of items and skills not in initial arrays
    
    Created Defaults.js to store items, and defaults.
    
    Updated game for testing
    
    Updated UserContext for performance and readability

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,25 +1,55 @@
-import { useContext, useEffect, useRef } from "react";
+import { useContext } from "react";
 import styled from "styled-components";
 import { userContext } from "../UserContext";
 
 const Game = () => {
+  const {
+    currentSkill,
+    setCurrentSkill,
+    player,
+    setPlayer,
+    playerDead,
+    setPlayerDead,
+    UPDATE_INVENTORY,
+    UPDATE_SKILL,
+    UPDATE_HEALTH,
+    UPDATE_WALLET,
+    UPDATE_EQUIPPED,
+    EQUIPMENT,
+    EQUIPMENT_STATS,
+  } = useContext(userContext);
+
+  const {
+    skills: { cooking },
+  } = player;
+
+  const handleButtonClick = () => {
+    UPDATE_SKILL("cooking", 50, player, setPlayer);
+    UPDATE_INVENTORY("scrap metal", 1, player, setPlayer);
+    UPDATE_EQUIPPED(EQUIPMENT_STATS["stick"], player, setPlayer);
+    UPDATE_HEALTH(-10, player, setPlayer, setPlayerDead);
+  };
 
   return (
-    <Wrapper></Wrapper>
+    <Wrapper>
+      {cooking.xp > 0 && cooking.xp}
+      <button onClick={handleButtonClick}>click me</button>
+    </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-width: 100vw;
-height: 100vh;
-background-color: aliceblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: aliceblue;
 `;
 
 const ItemContainer = styled.div`
   text-transform: capitalize;
   user-select: none;
 `;
+
 export default Game;
